cb3c0132116e3adf965f1a5de4a3f8e127ea8914
crazymay35 <89706779+crazymay35@users.noreply.github.com>
2026-09-12 12:08:40 -0600

    impl(0) implemented knight calc, starting on pawn

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,8 +9,63 @@ public class KnightMovesCalculator extends PieceMovesCalculator {
         super(board,position);
     }
 
+    protected enum Direction {
+        NULL(0,0),
+        UP_RIGHT(2,1),
+        UP_LEFT(2,-1),
+        RIGHT_UP(1,2),
+        RIGHT_DOWN(-1,2),
+        DOWN_RIGHT(-2,1),
+        DOWN_LEFT(-2,-1),
+        LEFT_UP(1,-2),
+        LEFT_DOWN(-1,-2);
+
+        private final int row;
+        private final int col;
+
+        Direction(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+        public ChessPosition step(ChessPosition position, int length) {
+            return new ChessPosition(position.getRow() + (this.row * length), position.getColumn() + (this.col * length));
+        }
+    }
+
+    private ChessMove validMove(Direction direction) {
+        ChessPiece piece = getBoard().getPiece(getPosition());
+        if (inBounds(direction.step(getPosition(), 1))) {
+            ChessPosition newPosition = direction.step(getPosition(), 1);
+            ChessPiece newPiece = getBoard().getPiece(newPosition);
+            if (pieceIsNull(newPosition) || teamColorNotMatch(newPiece, piece)) {
+                return new ChessMove(getPosition(), newPosition, null);
+            }
+        }
+        return null;
+    }
+
+    private void add(ChessMove move, Collection<ChessMove> possibleMoves) {
+        if (move != null) {
+            possibleMoves.add(move);
+        }
+    }
+
+    @Override
+    protected Collection<ChessMove> moveDirection(PieceMovesCalculator.Direction direction) {
+        List<ChessMove> possibleMoves = new ArrayList<>();
+        add(validMove(Direction.UP_RIGHT), possibleMoves);
+        add(validMove(Direction.UP_LEFT), possibleMoves);
+        add(validMove(Direction.RIGHT_UP), possibleMoves);
+        add(validMove(Direction.RIGHT_DOWN), possibleMoves);
+        add(validMove(Direction.DOWN_RIGHT), possibleMoves);
+        add(validMove(Direction.DOWN_LEFT), possibleMoves);
+        add(validMove(Direction.LEFT_UP), possibleMoves);
+        add(validMove(Direction.LEFT_DOWN), possibleMoves);
+        return possibleMoves;
+    }
+
     @Override
     protected Collection<ChessMove> pieceMoves() {
-        return List.of();
+        return moveDirection(PieceMovesCalculator.Direction.NULL);
     }
 }
--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -4,6 +4,23 @@ import java.util.Collection;
 import java.util.List;
 
 public class PawnMovesCalculator extends PieceMovesCalculator{
+    protected enum Direction {
+        FIRST_TURN(2,0),
+        UP(1,0),
+        ATTACK_RIGHT(1,1),
+        ATTACK_LEFT(1,-1);
+
+        private final int row;
+        private final int col;
+
+        Direction(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+        public ChessPosition step(ChessPosition position, int length) {
+            return new ChessPosition(position.getRow() + (this.row * length), position.getColumn() + (this.col * length));
+        }
+    }
     public PawnMovesCalculator (ChessBoard board, ChessPosition position) {
         super(board,position);
     }
