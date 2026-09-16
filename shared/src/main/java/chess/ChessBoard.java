0748b5cce9a2f0575c18ec27c8ce86d938a3cba3
crazymay35 <89706779+crazymay35@users.noreply.github.com>
2026-09-15 18:06:03 -0600

    impl(0) implemented chessboard reset

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,7 +1,6 @@
 package chess;
 
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -51,10 +50,46 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+
+        Collection<ChessPiece> whitePieces = List.of(
+                new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.ROOK),
+                new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.KNIGHT),
+                new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.BISHOP),
+                new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.QUEEN),
+                new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.KING),
+                new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.BISHOP),
+                new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.KNIGHT),
+                new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.ROOK)
+        );
+        addToBoard(whitePieces,1);
+        addPawn(ChessGame.TeamColor.WHITE, 2);
+
+        Collection<ChessPiece> blackPieces = List.of(
+                new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.ROOK),
+                new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.KNIGHT),
+                new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.BISHOP),
+                new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.QUEEN),
+                new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.KING),
+                new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.BISHOP),
+                new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.KNIGHT),
+                new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.ROOK)
+        );
+        addToBoard(blackPieces,8);
+        addPawn(ChessGame.TeamColor.BLACK, 7);
     }
 
-
+    private void addPawn(ChessGame.TeamColor teamColor, int row) {
+        for (int i = 1; i <= 8; i++) {
+            addPiece(new ChessPosition(row,i),new ChessPiece(teamColor,ChessPiece.PieceType.PAWN));
+        }
+    }
+    private void addToBoard(Collection<ChessPiece> pieces, int row) {
+        int column = 1;
+        for (ChessPiece piece : pieces) {
+            addPiece(new ChessPosition(row,column),piece);
+            column++;
+        }
+    }
 
     @Override
     public String toString() {
